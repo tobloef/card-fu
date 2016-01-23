9d0fb4cd8ff0cf552e47c28e11e83e3e58ed745f
Tobias Løfgren <tobloef@gmail.com>
2016-01-24 00:26:58 +0100

    Added mongodb

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var http = require("http").Server(app);
 var io = require("socket.io").listen(http);
 var favicon = require("serve-favicon");
 var debug = require("debug")("http");
+var mongo = require("mongodb");
+var monk = require("monk");
+var db = monk("localhost/cardjitsu");
 
 var gameHandler = require("./game.js");
 
@@ -11,8 +14,16 @@ app.set("port", (process.env.PORT || 3000));
 app.use(favicon(__dirname + "/public/images/node_favicon.ico"));
 app.use(express.static("public"));
 
+var users = db.get("users");
+
 app.get("/", function(req, res) {
 	// Will serve static page index.html
+});
+
+app.get("/mongo", function(req, res) {
+	users.find({}, {}, function(e, test) {
+		res.send(test);
+	});
 });
 
 // If page doesn't exist
