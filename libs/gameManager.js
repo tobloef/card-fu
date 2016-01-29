877510d0ae83cf1be6704101c4943100a68e1a81
Tobias Løfgren <tobloef@gmail.com>
2016-01-29 19:12:34 +0100

    Cards can now fight

--- libs/gameManager.js
+++ libs/gameManager.js
@@ -1,6 +1,7 @@
 var debug = require("debug")("game");
 var socketio = require("socket.io");
 var dbm = require("../libs/databaseManager");
+var elo = require("../libs/elo");
 
 var players = [];
 var queue = [];
@@ -11,137 +12,144 @@ numbers = [10, 8, 7, 6, 5, 5, 4, 3, 3, 2];
 
 defaultDeck = [];
 for (var t = 0; t < types.length; t++) {
-    for (var n = 1; n < numbers.length; n++) {
-        defaultDeck.push({
-            type: types[t],
-            number: numbers[n]
-        });
-    }
+	for (var n = 1; n < numbers.length; n++) {
+		defaultDeck.push({
+			type: types[t],
+			number: numbers[n]
+		});
+	}
 }
 
 
 //////////  Socket.io  \\\\\\\\\\
 module.exports.listen = function(app) {
-    io = socketio.listen(app);
-    io.on("connection", function(socket) {
+	io = socketio.listen(app);
+	io.on("connection", function(socket) {
 
 		socket.on("disconnect", function() {
+			debug(queue);
 			playerDisconnected(socket);
+			debug(queue);
 		});
 
-        socket.on("username submit", function(desiredUsername) {
-            processUsername(desiredUsername, socket);
-        });
+		socket.on("username submit", function(desiredUsername) {
+			processUsername(socket, desiredUsername);
+		});
 
-        socket.on("enter queue", function() {
-            enterQueue(socket);
-        });
-        
-        socket.on("leave queue", function() {
-            leaveQueue(socket);
-        });
+		socket.on("enter queue", function() {
+			enterQueue(socket);
+		});
 
-        socket.on("get user info", function(socket) {
-            sendUserInfo(socket);
-        });
-    });
-    return io;
+		socket.on("leave queue", function() {
+			leaveQueue(socket);
+		});
+
+		socket.on("get player info", function() {
+			sendPlayerInfo(socket);
+		});
+
+		socket.on("play card", function(index) {
+			playCard(socket, index);
+		});
+	});
+	return io;
 };
 
 
 //////////  Methods  \\\\\\\\\\
 function playerDisconnected(socket) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
 	var index = players.indexOf(findPlayerById(socket.id));
 	if (index > -1) {
 		players.splice(index, 1);
 	}
 	leaveQueue(socket);
+	sendStats();
 }
 
 function findPlayerByUsername(username) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
-    for (var i = 0; i < players.length; i++) {
-        if (players[i].username === username) {
-            return players[i];
-        }
-    }
-    return false;
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	for (var i = 0; i < players.length; i++) {
+		if (players[i].username === username) {
+			return players[i];
+		}
+	}
+	return false;
 }
 
-function findPlayerById(id) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
-    for (var i = 0; i < players.length; i++) {
-        if (players[i].socketId === id) {
-            return players[i];
-        }
-    }
-    return false;
+function findPlayerById(socketId) {
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	for (var i = 0; i < players.length; i++) {
+		if (players[i].socket.id === socketId) {
+			return players[i];
+		}
+	}
+	return false;
 }
 
 function playerExists(username) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
-    for (var i = 0; i < players.length; i++) {
-        if (players[i].username.toLowerCase() === username.toLowerCase()) {
-           return true;
-        }
-    }
-    return false;
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	for (var i = 0; i < players.length; i++) {
+		if (players[i].username.toLowerCase() === username.toLowerCase()) {
+			return true;
+		}
+	}
+	return false;
 }
 
-function processUsername(desiredUsername, socket) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
-    var doesExist = playerExists(desiredUsername);
-    if (!doesExist) {
-        players.push({
-            username: desiredUsername,
-            socketId: socket.id,
-            elo: 1000,
-            deck: defaultDeck
-        });
-        sendStats();
-        sendUserInfo(socket);
-    }
-    socket.emit("username response", {
-        exists: doesExist
-    });
+function processUsername(socket, desiredUsername) {
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	var doesExist = playerExists(desiredUsername);
+	if (!doesExist) {
+		players.push({
+			username: desiredUsername,
+			socket: socket,
+			elo: 1000,
+			deck: defaultDeck
+		});
+		sendStats();
+		sendPlayerInfo(socket);
+	}
+	socket.emit("username response", {
+		exists: doesExist
+	});
 }
 
 function sendStats() {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
-    stats = {
-        onlinePlayers: []
-    };
-    for (var i = 0; i < players.length; i++) {
-        stats.onlinePlayers.push(players[i].username);
-    }
-    io.emit("global stats", stats);
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	stats = {
+		onlinePlayers: []
+	};
+	for (var i = 0; i < players.length; i++) {
+		stats.onlinePlayers.push(players[i].username);
+	}
+	io.emit("stats", stats);
 }
 
-function sendUserInfo(socket) {
-    var user = findPlayerById(socket.id);
-    info = {
-    	username: user.username,
-    	elo: user.elo
-    };
-    socket.emit("user info", info);
+function sendPlayerInfo(socket) {
+	var player = findPlayerById(socket.id);
+	info = {
+		username: player.username,
+		elo: player.elo
+	};
+	socket.emit("player info", info);
 }
 
 
 function enterQueue(socket) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
-    var player = findPlayerById(socket.id);
-    if (queue.indexOf(player) === -1) {
-        queue.push(player);
-        socket.emit("queue entered");
-        if (queue.length >= 2) {
-            createMatch([queue.shift(), queue.shift()]);
-        }
-    }
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	var player = findPlayerById(socket.id);
+	if (queue.indexOf(player) === -1) {
+		queue.push(player);
+		socket.emit("queue entered");
+		if (queue.length >= 2) {
+			createMatch([queue.shift(), queue.shift()]);
+		}
+	}
 }
 
 function leaveQueue(socket) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
 	var index = queue.indexOf(findPlayerById(socket.id));
 	if (index > -1) {
 		queue.splice(index, 1);
@@ -150,73 +158,184 @@ function leaveQueue(socket) {
 }
 
 function createMatch(participents) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
-    usernames = [];
-    var matchId = createId();
-    matches[matchId] = {
-        players: [],
-        status: 0,
-        modifier: 0,
-        timer: undefined
-    };
-    for (var i = 0; i < participents.length; i++) {
-    	var playerObject = {
-            username: participents[i].username,
-            socketId: participents[i].socketId,
-            elo: participents[i].elo,
-            deck: shuffleDeck(participents[i].deck),
-            cards: [],
-            points: []
-        }
-        var socket = io.sockets.connected[participents[i].socketId]
-        dealInitialCards(playerObject);
-        matches[matchId].players.push(playerObject);
-        usernames.push(participents[i].username);
-        socket.emit("initial hand", playerObject.cards);
-        socket.join(matchId);
-    }
-    io.to(matchId).emit("enter match", usernames);
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	usernames = [];
+	var id = createId();
+	match = {
+		matchId: id,
+		players: [],
+		status: 0,
+		modifier: 0,
+		timer: undefined
+	};
+	for (var i = 0; i < participents.length; i++) {
+		var playerObject = {
+			username: participents[i].username,
+			socket: participents[i].socket,
+			elo: participents[i].elo,
+			deck: shuffleDeck(participents[i].deck),
+			cards: [],
+			currentCard: undefined,
+			points: []
+		}
+		dealInitialCards(playerObject);
+		match.players.push(playerObject);
+		usernames.push(participents[i].username);
+		participents[i].socket.emit("initial hand", playerObject.cards);
+		participents[i].socket.join(id);
+	}
+	matches.push(match);
+	io.to(id).emit("enter match", usernames);
 }
 
 function createId() {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
-    var id = "";
-    var charset = "ABCDEFGHIJKLMNOPQRSTUCWXYZabcdefghijklmnopqrtsuvwxyz1234567890";
-    for (var i = 0; i < 16; i++) {
-        id += charset.charAt(Math.floor(Math.random() * charset.length));
-    }
-    return id;
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	var id = "";
+	var charset = "ABCDEFGHIJKLMNOPQRSTUCWXYZabcdefghijklmnopqrtsuvwxyz1234567890";
+	for (var i = 0; i < 16; i++) {
+		id += charset.charAt(Math.floor(Math.random() * charset.length));
+	}
+	return id;
 }
 
 function dealInitialCards(playerObject) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
-    for (var i = 0; i < 5; i++) {
-    	playerObject.cards[i] = drawCard(playerObject.deck);
-    }
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	for (var i = 0; i < 5; i++) {
+		playerObject.cards[i] = drawCard(playerObject.deck);
+	}
 }
 
 function drawCard(deck) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
-    if (deck.length > 0) {
-    	return deck.shift();
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	if (deck.length > 0) {
+		return deck.shift();
 	} else {
 		return false;
 	}
 }
 
 function shuffleDeck(deck) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
-    deckCopy = deck.slice();
-    for (var i = deckCopy.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = deckCopy[i];
-        deckCopy[i] = deckCopy[j];
-        deckCopy[j] = temp;
-    }
-    return deckCopy;
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	deckCopy = deck.slice();
+	for (var i = deckCopy.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = deckCopy[i];
+		deckCopy[i] = deckCopy[j];
+		deckCopy[j] = temp;
+	}
+	return deckCopy;
+}
+
+function findMatchBySocketId(socketId, returnUser) {
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	for (var i = 0; i < matches.length; i++) {
+		for (var j = 0; j < matches[i].players.length; j++) {
+			if (matches[i].players[j].socket.id === socketId) {
+				if (returnUser) {
+					return [matches[i], matches[i].players[j]];
+				} else {
+					return matches[i];
+				}
+			}
+		}
+	}
+}
+
+function playCard(socket, index) {
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	var result = findMatchBySocketId(socket.id, true);
+	var match = result[0];
+	var user = result[1];
+	user.currentCard = user.cards[index];
+	user.cards[index] = undefined;
+	if (currentCardsReady(match)) {
+		fightCards(match);
+	}
+}
+
+function currentCardsReady(match) {
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	for (var i = 0; i < match.players.length; i++) {
+		if (match.players[i].currentCard === undefined) {
+			return false;
+		}
+	}
+	return true;
+}
+
+//This function is hell, make it better
+function fightCards(match) {
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	var player1 = match.players[0];
+	var player2 = match.players[1];
+
+	if (player1.currentCard.type === "fire") {
+		if (player2.currentCard.type === "fire") {
+			if (player1.currentCard.number > player2.currentCard.number) {
+				processResults(match, false, player1, player1);
+			} else if (player1.currentCard.number < player2.currentCard.number) {
+				processResults(match, false, player2, player1);
+			} else {
+				processResults(match, true, player2, player1);
+			}
+		} else if (player2.currentCard.type === "ice") {
+			processResults(match, false, player1, player2);
+		} else if (player2.currentCard.type === "water") {
+			processResults(match, false, player2, player1);
+		}
+	} else if (player1.currentCard.type === "ice") {
+		if (player2.currentCard.type === "fire") {
+			processResults(match, false, player2, player1);
+		} else if (player2.currentCard.type === "ice") {
+			if (player1.currentCard.number > player2.currentCard.number) {
+				processResults(match, false, player1, player1);
+			} else if (player1.currentCard.number < player2.currentCard.number) {
+				processResults(match, false, player2, player1);
+			} else {
+				processResults(match, true, player2, player1);
+			}
+		} else if (player2.currentCard.type === "water") {
+			processResults(match, false, player1, player2);
+		}
+	} else if (player1.currentCard.type === "water") {
+		if (player2.currentCard.type === "fire") {
+			processResults(match, false, player1, player2);
+		} else if (player2.currentCard.type === "ice") {
+			processResults(match, false, player2, player1);
+		} else if (player2.currentCard.type === "water") {
+			if (player1.currentCard.number > player2.currentCard.number) {
+				processResults(match, false, player1, player1);
+			} else if (player1.currentCard.number < player2.currentCard.number) {
+				processResults(match, false, player2, player1);
+			} else {
+				processResults(match, true, player2, player1);
+			}
+		}
+	}
+}
+
+//winner and looser parameter names only applicable is not tied.
+function processResults(match, tied, winner, loser) {
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	if (!tied) {
+		
+	} else {
+
+	}
+	newTurn(match);
+}
+
+function checkPoints(user) {
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	
+}
+
+function newTurn(match) {
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+
 }
 
 function func(args) {
-    debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
+	debug("%s(%s)", arguments.callee.name, Array.prototype.slice.call(arguments).sort());
 
 }
--- public/javascripts/game_client.js
+++ public/javascripts/game_client.js
@@ -4,27 +4,27 @@ submitUsername(prompt("Please enter a username:"));
 
 //////////  Socket Events  \\\\\\\\\\
 socket.on("username response", function(response) {
-    if (!response.exists) {
-        displayMainScreen();
-    } else {
-        submitUsername(prompt("Username already exists, please enter a different one:"));
-    }
+	if (!response.exists) {
+		displayMainScreen();
+	} else {
+		submitUsername(prompt("Username already exists, please enter a different one:"));
+	}
 });
 
-socket.on("user info", function(stats) {
-    displayUserInfo(stats);
+socket.on("player info", function(stats) {
+	displayPlayerInfo(stats);
 });
 
 socket.on("stats", function(stats) {
-    displayStats(stats);
+	displayStats(stats);
 });
 
 socket.on("enter match", function(usernames) {
-    prepareForMatch(usernames);
+	prepareForMatch(usernames);
 });
 
 socket.on("queue entered", function() {
-    queueEntered();
+	queueEntered();
 });
 
 socket.on("queue left", function() {
@@ -32,7 +32,7 @@ socket.on("queue left", function() {
 });
 
 socket.on("initial hand", function(cards) {
-    displayInitialHand(cards);
+	displayInitialHand(cards);
 });
 
 //////////  jQuery Events  \\\\\\\\\\
@@ -41,90 +41,90 @@ $("#queue_button").click(enterQueue);
 $("#clear_button").click(clearLog);
 
 $(".card_button").click(function() {
-    playCard(this.id);
+	playCard(this.id);
 });
 
 
 //////////  Methods  \\\\\\\\\\
 function submitUsername(desiredUsername) {
-    console.log("%s", arguments.callee.name);
-    socket.emit("username submit", desiredUsername);
+	console.log("%s", arguments.callee.name);
+	socket.emit("username submit", desiredUsername);
 }
 
 function getStats() {
-    console.log("%s", arguments.callee.name);
-    socket.emit("get stats");
+	console.log("%s", arguments.callee.name);
+	socket.emit("get stats");
 }
 
 function displayStats(stats) {
-    console.log("%s", arguments.callee.name);
-    $("#player_list_header").text(stats.onlinePlayers.length + " players online:");
-    $("#player_list").empty();
-    for (var i = 0; i < stats.onlinePlayers.length; i++) {
-        $("#player_list").append("<li>" + stats.onlinePlayers[i] + "</li>");
-    }
+	console.log("%s", arguments.callee.name);
+	$("#player_list_header").text(stats.onlinePlayers.length + " players online:");
+	$("#player_list").empty();
+	for (var i = 0; i < stats.onlinePlayers.length; i++) {
+		$("#player_list").append("<li>" + stats.onlinePlayers[i] + "</li>");
+	}
 }
 
-function getUserInfo() {
-    console.log("%s", arguments.callee.name);
+function getPlayerInfo() {
+	console.log("%s", arguments.callee.name);
 	socket.emit("get user info");
 }
 
-function displayUserInfo(info) {
-    console.log("%s", arguments.callee.name);
-    $("#user_info").text("Hello " + info.username + ", you have an ELO of " + info.elo + ".");
+function displayPlayerInfo(info) {
+	console.log("%s", arguments.callee.name);
+	$("#user_info").text("Hello " + info.username + ", you have an ELO of " + info.elo + ".");
 }
 
 function displayMainScreen() {
-    console.log("%s", arguments.callee.name);
-    $("#main_screen").css("visibility", "visible");
+	console.log("%s", arguments.callee.name);
+	$("#main_screen").css("visibility", "visible");
 }
 
 function enterQueue() {
-    console.log("%s", arguments.callee.name);
+	console.log("%s", arguments.callee.name);
 	socket.emit("enter queue");
 	$("#queue_button").html("Leave Queue");
 	$("#queue_button").off("click").on("click", leaveQueue);
 }
 
 function leaveQueue() {
-    console.log("%s", arguments.callee.name);
+	console.log("%s", arguments.callee.name);
 	socket.emit("leave queue");
 	$("#queue_button").html("Enter Queue");
 	$("#queue_button").off("click").on("click", enterQueue);
 }
 
 function queueEntered() {
-    console.log("%s", arguments.callee.name);
-    $("#log").append("<li>" + "Entered the queue. Waiting for an opponent..." + "</li>");
+	console.log("%s", arguments.callee.name);
+	$("#log").append("<li>" + "Entered the queue. Waiting for an opponent..." + "</li>");
 }
 
 function queueLeft() {
-    console.log("%s", arguments.callee.name);
-    $("#log").append("<li>" + "Left the queue." + "</li>");
+	console.log("%s", arguments.callee.name);
+	$("#log").append("<li>" + "Left the queue." + "</li>");
 }
 
 function prepareForMatch(usernames) {
-    console.log("%s", arguments.callee.name);
-    $("#log").append("<li>" + "Match started " + usernames.join(" vs ") + "</li>");
-    $("#match_screen").css("visibility", "visible");
+	console.log("%s", arguments.callee.name);
+	$("#log").append("<li>" + "Match started " + usernames.join(" vs ") + "</li>");
+	$("#match_screen").css("visibility", "visible");
 }
 
 function clearLog() {
-    console.log("%s", arguments.callee.name);
+	console.log("%s", arguments.callee.name);
 	$("#log").empty();
 }
 
 function displayInitialHand(cards) {
-    console.log("%s", arguments.callee.name);
-    $(".card_button").css("visibility", "visible");
-    for (var i = 0; i < cards.length; i++) {
-        $(".card_button#" + i).html(cards[i].type + " " + cards[i].number);
-    }
+	console.log("%s", arguments.callee.name);
+	$(".card_button").css("visibility", "visible");
+	for (var i = 0; i < cards.length; i++) {
+		$(".card_button#" + i).html(cards[i].type + " " + cards[i].number);
+	}
 }
 
 function playCard(index) {
-    console.log("%s", arguments.callee.name);
-    socket.emit("play card", index);
-    $(".card_button#" + index).html("");
+	console.log("%s", arguments.callee.name);
+	socket.emit("play card", index);
+	$(".card_button#" + index).html("");
 }
