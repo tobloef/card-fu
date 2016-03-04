a2b1bf059d396bf34ceecfada23dfb3325d5f9f2
Tobias Løfgren <tobloef@gmail.com>
2016-03-04 04:57:38 -0500

    Switched to underscore file naming convention

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
-var io = require("./libs/gameManager").listen(http);
+var io = require("./libs/game_manager").listen(http);
 
 app.set("port", (process.env.PORT || 3001));
 app.use(express.static("public"));
